f12145e1bf865cdcd8ff8f577dadac702ca697af
MSI\roywi <roywilliamvermeulen@gmail.com>
2022-09-28 18:10:49 +0200

    added basic features to the baseframework, Like some plain css and php includes

---
+++ app/views/includes/navbar.php
@@ -0,0 +1,9 @@
+<h3 class="titles"><?= $data['title']; ?></h3>
+<nav>
+    <ul class="nav-links">
+        <li>
+            <a href="/Home/index">Home</a>
+        </li>
+    </ul>
+</nav>
+<a href="..."><button>Login</button></a>
--- app/views/index.php
+++ app/views/index.php
@@ -1,7 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-HOME PAGE<br>
-<a href="<?=URLROOT;?>/gcontrollers/dashboard">dashboard</a>
+<head>
+    <?php require_once APPROOT . 'views/includes/head.php'; ?>
+</head>
 
+<body>
+    <header>
+        <?php require_once APPROOT . 'views/includes/navbar.php'; ?>
+    </header>
+    <main>
 
+    </main>
+    <footer>
+        <?php require_once APPROOT . 'views/includes/footer.php'; ?>
+    </footer>
+</body>
+
+</html>
